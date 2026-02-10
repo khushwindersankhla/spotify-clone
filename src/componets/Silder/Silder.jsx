fdf8ab65b61eca7011709a372c1fcca97374ccdb
khushwinder <khushwindersanhla02gamil.com>
2026-02-10 11:31:45 +0530

    file-upload

---
+++ src/componets/Silder/Silder.jsx
@@ -0,0 +1,15 @@
+import './Silder.css'
+function Silder() {
+    return (
+        <>
+            <div className="silder-man">
+                <div className="silder-one">
+
+                </div>
+                <div className="silder-two"> 
+                     <div className="Silder-text"><h2>Trading Songs</h2></div></div>
+            </div>
+        </>
+    )
+}
+export default Silder;
